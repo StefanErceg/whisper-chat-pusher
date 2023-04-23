36919da3c60104b41ee900f3f67ee6c0442e0da6
Stefan Erceg <stefanerceg95@gmail.com>
2023-04-23 21:30:23 +0200

    add initial rabbit communication, endpoint for sending messages, move everything to src folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
+
 import { router } from './router';
 import { connect } from './rabbit';
 
@@ -9,14 +11,11 @@ const app = express();
 const port = process.env.PORT;
 const version = process.env.VERSION;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`/api/${version}`, router);
-
-app.get('/', (req: Request, res: Response) => {
-	res.send('Whisper chat pusher');
-});
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
